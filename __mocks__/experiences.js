37be93f3fdfceb9e6d34cbecc5e93631b0252247
tigger <fronttigger@gmail.com>
2021-04-22 00:18:46 +0900

    20210422

--- __mocks__/experiences.js
+++ __mocks__/experiences.js
@@ -3,7 +3,7 @@ const experiences = [
     id: 1,
     company: '보닥(아이지넷)',
     job: 'Frontend Engineer',
-    dueDate: '2020. 12 - 2021. 04',
+    dueDate: '2020. 12 - 현재',
     projects: [
       {
         id: 1,
@@ -62,12 +62,12 @@ const experiences = [
         dueDate: '2019. 11 - 2020. 4',
         description: `KCC의 자회사 HomeCC의 웹 애플리케이션을 개발하였습니다. 백엔드 및 프론트 개발을 모두 경험하였습니다.`,
         todos: [
-          '오프라인 매장 관리 기능 개발',
-          '인테리어 관리 기능 개발',
-          '이벤트 관리 기능 개발',
-          '리모델링 신청 폼 개발',
-          '고객센터 기능 개발',
-          'Kakao API 연동하여 지도 개발',
+          '오프라인 매장 관리 기능',
+          '인테리어 관리 기능',
+          '이벤트 관리 기능',
+          '리모델링 신청 폼',
+          '고객센터 기능',
+          'Kakao API 지도 기능',
           '기타 유지보수',
         ],
         stacks: [
@@ -79,7 +79,7 @@ const experiences = [
           'Thymeleaf',
           'jQuery',
           'Postgresql',
-          'Gitlab',
+          'GitLab',
         ],
       },
       {
@@ -90,10 +90,10 @@ const experiences = [
                 개발했습니다. 프로젝트 기간동안 백오피스 개발을 하였으며 비즈니스
                 요구에 맞는 기능들을 구현하였습니다.`,
         todos: [
-          '매장 관리 기능 개발',
-          '제품 관리 기능 개발',
-          '포인트 적립 및 관리 기능 개발',
-          '회원 알림 기능 개발',
+          '매장 관리 기능',
+          '제품 관리 기능',
+          '포인트 적립 및 관리 기능',
+          '회원 알림 기능',
         ],
         stacks: [
           'Java 8',
@@ -104,7 +104,7 @@ const experiences = [
           'Thymeleaf',
           'jQuery',
           'Mysql',
-          'Gitlab',
+          'GitLab',
         ],
       },
     ],
